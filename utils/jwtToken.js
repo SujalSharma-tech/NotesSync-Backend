22c29a527362d74c8078e18eebca2f1779cb9a0f
sujal <sharmasujal995@gmail.com>
2024-07-21 20:13:52 +0530

    changed max-age value to 7 days

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,9 +1,9 @@
 export const sendToken = (user, statusCode, message, res) => {
   const token = user.getJWTToken();
   const options = {
-    expiresIn: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000
-    ),
+    // expiresIn: new Date(
+    //   Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 1000
+    // ),
     httpOnly: true,
     secure: true,
     sameSite: "none",
